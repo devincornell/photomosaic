9fbe00df933ade8bb679a7aefe4192ef0632d807
Devin J. Cornell <devincornell@github.com>
2023-11-08 12:47:33 -0500

    worked on basics of image grid

--- canvas/image/imagegrid.py
+++ canvas/image/imagegrid.py
@@ -6,97 +6,103 @@ import numpy as np
 import pathlib
 
 if typing.TYPE_CHECKING:
+    from .image import Image, Height, Width
     from .subimage import SubImage
 
-from .image import Image, Height, Width
-
-class X(int):
+class GridX(int):
     pass
 
-class Y(int):
+class GridY(int):
     pass
 
 @dataclasses.dataclass
 class ImageGrid:
-    images: typing.List[Image]
+    subimages: typing.List[SubImage]
     y_divisions: int
-    image_width: Width
-    image_height: Height
 
     ################## factory method constructors ##################
     @classmethod
     def from_fixed_subimages(cls, image: Image, subimage_height: Height, subimage_width: Width) -> typing.List[Image]:
-        '''Create by creating a grid of fixed-size sqares that fill the iamge.'''
+        '''Create by creating a grid of fixed-size sqares that fill the image.'''
         full_h,full_w = image.size
-        y_divisions = full_h // subimage_height
-        x_divisions = full_w // subimage_width
-        subimages = []
-        for iy in range(y_divisions):
-            for ix in range(x_divisions):
-                sc = image.slice(
-                    y = iy*subimage_height, 
-                    x = ix*subimage_width, 
-                    h = subimage_height, 
-                    w = subimage_width,
-                )
-                subimages.append(sc)
-        return cls(
-            images = subimages, 
-            y_divisions = y_divisions,
-            image_height = full_h,
-            image_width = full_w,
+        return cls.from_specs(
+            image = image,
+            y_divisions = full_h // subimage_height,
+            x_divisions = full_w // subimage_width,
+            subimage_height = subimage_height,
+            subimage_width = subimage_width,
         )
     
     @classmethod
     def from_even_divisions(cls, image: Image, y_divisions: int, x_divisions: int) -> typing.List[Image]:
         '''Create by dividing up a single canvas into equal parts.'''
-        h,w = image.size
-        x_size = w // x_divisions
-        y_size = h // y_divisions
+        full_h,full_w = image.size
+        return cls.from_specs(
+            image = image,
+            y_divisions = y_divisions,
+            x_divisions = x_divisions,
+            subimage_height = full_h // y_divisions,
+            subimage_width = full_w // x_divisions,
+        )
+    
+    @classmethod
+    def from_specs(cls, image: Image, y_divisions: int, x_divisions: int, subimage_height: int, subimage_width: int) -> typing.List[SubImage]:
+        '''Create a list of subimages that are empty.'''
         subimages = []
         for iy in range(y_divisions):
             for ix in range(x_divisions):
-                sc = image.slice(iy*y_size, ix*x_size, y_size, x_size)
-                subimages.append(sc)
+                subimages.append(SubImage.from_image(
+                    image = image,
+                    y = iy*subimage_height, 
+                    x = ix*subimage_width, 
+                    h = subimage_height, 
+                    w = subimage_width,
+                ))
         return cls(
-            images = subimages, 
+            subimages = subimages, 
             y_divisions = y_divisions,
-            image_height = h,
-            image_width = w,
         )
-
+    
     ################## Dunder ##################
     def __getitem__(self, ind: typing.Union[typing.Tuple[Y,X],int]) -> Image:
         '''Get image at (y,x) location.'''
         y,x = ind
         if y >= self.y_divisions:
             raise IndexError(f'y index {y} is gt or eq to width {self.y_divisions}')
-        return self.images[x*self.y_divisions + y]
+        return self.subimages[self.xy_to_ind(y,x,self.y_divisions)]
+
+    ################## Grid mappings ##################
+    @staticmethod
+    def ind_to_xy(ind: int, y_divisions: int) -> typing.Tuple[GridY,GridX]:
+        '''Converts an index to a grid location.'''
+        return ind // y_divisions, ind % y_divisions
+    
+    @staticmethod
+    def xy_to_ind(y: GridY, x: GridX, y_divisions) -> int:
+        '''Converts a grid location to an index.'''
+        return y*y_divisions + x
     
     ################## Properties ##################
     @property
-    def full_shape(self) -> int:
-        return self.y_divisions * self.x_slices
+    def full_size(self) -> typing.Tuple[Height, Width]:
+        sub_h, sub_w = self.sub_size
+        return  sub_h*self.y_divisions, sub_w*self.x_divisions
 
     @property
-    def x_slices(self) -> int:
-        return len(self.images) // self.y_slices
-    
-    ################## Saving ##################
-    def to_image(cls, image_grid: ImageGrid, width: int) -> Image:
-        '''Reconstruct a canvas from a list of subcanvases.'''
-        ch,cw = scanvases[0].size
-        full_w = width * cw
-        full_h = (len(scanvases) // width) * ch
-        im = np.zeros((full_h,full_w,3), dtype=np.float64)
-        for i,sc in enumerate(scanvases):
-            grid_y, grid_x = i // width, i % width
-            im[grid_y*ch:(grid_y+1)*ch, grid_x*cw:(grid_x+1)*cw, :] = sc.im
-        return cls(
-            fpath=None,
-            im=im,
-        )
+    def sub_size(self) -> typing.Tuple[Height, Width]:
+        return self.subimages[0].window.h, self.subimages[0].window.w
 
+    @property
+    def x_divisions(self) -> int:
+        return len(self.subimages) // self.y_divisions
+
+    ################## Saving ##################
+    def to_image(self) -> Image:
+        '''Reconstruct a canvas from a list of subcanvases.'''
+        im = np.zeros(self.full_size, dtype=np.float64)
+        for i,si in enumerate(self.subimages):
+            im[si.window.y:si.window.y+si.window.h, si.window.x:si.window.x+si.window.w, :] = si.im
+        return Image(im=im)
 
 if __name__ == '__main__':
     ig = ImageGrid([])
--- canvas/image/subimage.py
+++ canvas/image/subimage.py
@@ -5,24 +5,31 @@ import skimage
 import numpy as np
 import pathlib
 
-from .image import Image
+from .image import Image, Height, Width
+
+class PixelX(int):
+    pass
+
+class PixelY(int):
+    pass
+
+@dataclasses.dataclass
+class Window:
+    y: PixelY
+    x: PixelX
+    h: Height
+    w: Width
 
 @dataclasses.dataclass(frozen=True)
 class SubImage(Image):
-    x: int
-    y: int
-    h: int
-    w: int
+    window: Window
 
     @classmethod
     def from_image(cls, image: Image, y: int, x: int, h: int, w: int) -> Image:
         '''Create a subimage from a square of the original canvas.'''
         return cls(
-            im = image.im[y:y+h,x:x+w],
-            y=y,
-            x=x,
-            h=h,
-            w=w,
+            im = image.im[y:y+h,x:x+w], 
+            window = Window(y=y, x=x, h=h, w=w),
         )
 
 
