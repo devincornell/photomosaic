6c5f4a00e3ff1534a09f94743bb8a3d51d315481
Devin J. Cornell <devincornell@github.com>
2023-11-15 09:52:22 -0500

    set up to return n largest distances for each position

--- canvas/distancedb.py
+++ canvas/distancedb.py
@@ -1,8 +1,11 @@
+from __future__ import annotations
+
 import doctable
 import dataclasses
 import pathlib
 import sqlalchemy
 
+@dataclasses.dataclass
 class DistanceDB:
     core: doctable.ConnectCore
     tab: doctable.DBTable
@@ -13,7 +16,12 @@ class DistanceDB:
         with core.begin_ddl() as ddl:
             tab = ddl.create_table_if_not_exists(Distance)
         return cls(core=core, tab=tab)
+    
+    @property
+    def q(self):
+        return Query(db=self)
 
+@dataclasses.dataclass
 class Query:
     db: DistanceDB
     
@@ -26,10 +34,23 @@ class Query:
         ) 
         SELECT target_path, position, thumb, distance FROM cte 
         WHERE r <= {n} 
-        ORDER BY distance ASC;'
+        ORDER BY distance ASC;
     '''
-    def select_top_distances(n: int) -> sqlalchemy.RowProxy:
-        return self.db.core.execute_sql(self.top_distances_sql.format(n=n))
+    def select_top_distances(self, n: int) -> sqlalchemy.RowProxy:
+        results = self.select_top_distances_sql(n)
+        #return results.all()[0]
+        return [Distance(**r._mapping) for r in results.all()]
+    
+    def select_top_distances_sql(self, n: int) -> sqlalchemy.RowProxy:
+        core, t = self.core_table()
+        return core.query().execute_sql(self.top_distances_sql.format(n=n))
+    
+    def count_by_target(self) -> int:
+        core, t = self.core_table()
+        return core.query().select([t['target_path'], doctable.f.count(t['id'])], group_by=[t['target_path']])
+    
+    def core_table(self) -> doctable.DBTable:
+        return self.db.core, self.db.tab
     
 
 @doctable.table_schema(
